d71094433c74200fe4c857c0b4477fcf3f708a91
therealsmack <andiaaes@hotmail.com>
2016-09-30 12:10:46 +0200

    added products, not only empthy classes (reverted from commit d5a69361a70b510b021a798587364add9f2d606d) (reverted from commit 7707d3290a307406670873052060776fe0349695)

--- ReviewCrawler/ReviewCrawler/Products/Motherboard.cs
+++ ReviewCrawler/ReviewCrawler/Products/Motherboard.cs
@@ -8,5 +8,19 @@ namespace ReviewCrawler.Products
 {
     class Motherboard
     {
+        string formFactor;
+        string chipset;
+        bool netCard;
+        bool soundCard;
+        bool graphicsCard;
+        bool multiGpu;
+        bool crossfire;
+        string cpuType;
+        int cpuCount;
+        string socket;
+        string sli;
+        string maxMem;
+        string memSlots;
+        string memType;
     }
 }
