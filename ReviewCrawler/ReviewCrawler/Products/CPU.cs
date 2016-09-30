a091f35ab1cba35906ce893aa19d55dac5274d3c
therealsmack <andiaaes@hotmail.com>
2016-09-30 12:44:40 +0200

    added products, not only empthy classes (reverted from commit d5a69361a70b510b021a798587364add9f2d606d) (reverted from commit 7707d3290a307406670873052060776fe0349695) (reverted from commit d71094433c74200fe4c857c0b4477fcf3f708a91)

--- ReviewCrawler/ReviewCrawler/Products/CPU.cs
+++ ReviewCrawler/ReviewCrawler/Products/CPU.cs
@@ -8,15 +8,5 @@ namespace ReviewCrawler.Products
 {
     class CPU
     {
-        string formFactor;
-        string model;
-        string clock;
-        bool StockCooler;
-        string cpuType;
-        int physicalCores;
-        int logicalCores;
-        string maxTurbo;
-        string integratedGpu;
-        string manufacturer;
     }
 }
