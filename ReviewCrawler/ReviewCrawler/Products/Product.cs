8781d9b0f9fed80736f2978b0a42d74aee6fcc7a
maltje13 <maltje13@student.aau.dk>
2016-11-16 13:56:09 +0100

    dsd

--- ReviewCrawler/ReviewCrawler/Products/Product.cs
+++ ReviewCrawler/ReviewCrawler/Products/Product.cs
@@ -150,13 +150,12 @@ namespace ReviewCrawler.Products
         private void InsertProductRetailer(Retailer retailer, int RID, int PID)
         {
             MySqlCommand command = new MySqlCommand("INSERT INTO Product_Retailer" +
-          "(productID, retailerID, url, price, priceDate)" +
-          "VALUES(@productID, @retailerID, @url, @price, @priceDate)", connection);
+          "(productID, retailerID, url, price)" +
+          "VALUES(@productID, @retailerID, @url, @price)", connection);
             command.Parameters.AddWithValue("@productID", PID);
             command.Parameters.AddWithValue("@retailerID", RID);
             command.Parameters.AddWithValue("@url", retailer.url);
             command.Parameters.AddWithValue("@price", retailer.price);
-            command.Parameters.AddWithValue("@priceDate", DateTime.Now);
 
             command.ExecuteNonQuery();
         }
